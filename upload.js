d3e272db5461942aebcedbaed3b9aac592e6105c
thangasblockchain <thangarajm@icloud.com>
2020-07-11 15:13:09 +0530

    test commit

---
+++ upload.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const AWS = require('aws-sdk');
+// Enter copied or downloaded access ID and secret key here
+const ID = '<copiedID>';
+const SECRET = '<copied secret key>';
+const s3 = new AWS.S3({
+    accessKeyId: ID,
+    secretAccessKey: SECRET
+});
+
+const uploadFile = (fileName) => {
+    // Read content from the file
+    const fileContent = fs.readFileSync(fileName);
+
+    // Setting up S3 upload parameters
+    const params = {
+        Bucket: 'thanga-test',
+        Key: 'thanga.ytty', // File name you want to save as in S3
+        Body: fileContent
+    };
+
+    // Uploading files to the bucket
+    s3.upload(params, function(err, data) {
+        if (err) {
+            throw err;
+        }
+        console.log(`File uploaded successfully. ${data.Location}`);
+    });
+};
+
+uploadFile('thanga.txt');
+
